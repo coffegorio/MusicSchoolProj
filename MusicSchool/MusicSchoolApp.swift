6bedaa67f4946ede55a5fff8ff14e91d30740ac3
coffegorio <coffegorio@vk.com>
2025-09-03 03:49:10 +0300

    Initial Commit

---
+++ MusicSchool/MusicSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSchoolApp.swift
+//  MusicSchool
+//
+//  Created by Egorio on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
